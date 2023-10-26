e6717d8d4781259d98cad02b06ceb286ea0b3ca2
Chris8855 <chris@englishtutorslive.com>
2023-10-26 12:41:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+/*
+ * @name Pointillism
+ * @description By Dan Shiffman. Mouse horizontal location controls size of
+ * dots. Creates a simple pointillist effect using ellipses colored according
+ * to pixels in an image.
+ * <p><em><span class="small"> To run this example locally, you will need an
+ * image file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">
+ * local server</a>.</span></em></p>
+ */
+let img;
+let smallPoint, largePoint;
+
+function preload() {
+  img = loadImage('Users/chris8855/Desktop/p5_project_showcase/ Pointillism/img.png');
+  image(0,0,650,400)
+}
+
+function setup() {
+  createCanvas(650, 400);
+  smallPoint = 4;
+  largePoint = 40;
+
+  noStroke();
+  background(255);
+  img.loadPixels();
+  imageMode(CENTER);
+  frameRate(9000);
+}
+
+function draw() {
+  img.resize(650, 400);
+  let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  let x = floor(random(img.width));
+  let y = floor(random(img.height));
+  let pix = img.get(x, y);
+  fill(pix, 0);
+  ellipse(x, y, pointillize, pointillize);
+
+}
